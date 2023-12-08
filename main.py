31de21e3c93a63e2d4338517adf868737056d19e
bryan <bdino673@gmail.com>
2023-12-08 20:44:24 +0100

    changes to the code

--- main.py
+++ main.py
@@ -3,3 +3,5 @@ from src.apriori import apriori
 if __name__ == '__main__':
     result = apriori([[1, 2, 3], [1, 2, 3], [1, 2, 3]], 0.3, 0.7)
     print(result)
+
+#This was a test Right and my final commit
